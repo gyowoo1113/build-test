26a8a94d5ed4b9bf8d644b142d5e767832d111ad
gyowoo1113 <yereube@naver.com>
2023-11-09 10:34:57 +0900

    Feat, Add : ModelAndView Controller 추가, view page로 사용할 home.html 추가

--- jpah2/src/main/java/com/yedam/app/CustomerController.java
+++ jpah2/src/main/java/com/yedam/app/CustomerController.java
@@ -3,6 +3,7 @@ package com.yedam.app;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
 
 import com.yedam.app.accessigndatajpa.Customer;
 import com.yedam.app.accessigndatajpa.CustomerRepository;
@@ -12,6 +13,15 @@ public class CustomerController {
 	
 	@Autowired
 	CustomerRepository repo;
+	
+	// RestController에서도 View Page로 넘어갈 수 있음
+	@GetMapping("/")
+	public ModelAndView main() {
+		// Model + View 객체
+		ModelAndView mv = new ModelAndView("home");
+		mv.addObject("greet","안녕하세요");
+		return mv;
+	}
 	
 	@GetMapping("/customer")
 	public Iterable<Customer> findall(){
