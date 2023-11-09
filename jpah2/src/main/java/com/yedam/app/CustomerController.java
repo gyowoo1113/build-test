c7639bdc71d38975ebb3d22b7df0c89627c49127
gyowoo1113 <yereube@naver.com>
2023-11-09 10:10:24 +0900

    Style : 실행을 위해 폴더 경로 설정 변경

---
+++ jpah2/src/main/java/com/yedam/app/CustomerController.java
@@ -0,0 +1,20 @@
+package com.yedam.app;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.yedam.app.accessigndatajpa.Customer;
+import com.yedam.app.accessigndatajpa.CustomerRepository;
+
+@RestController	// Controller, ResponseBody
+public class CustomerController {
+	
+	@Autowired
+	CustomerRepository repo;
+	
+	@GetMapping("/customer")
+	public Iterable<Customer> findall(){
+		return repo.findAll();
+	}
+}
